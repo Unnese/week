1f57476dc88db056dcb6ff6c2ade4b2c812082b3
Unnese <153504192+Unnese@users.noreply.github.com>
2024-01-25 21:35:00 +0300

    Add files via upload

---
+++ week/week/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресение"};
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Здравствуйте, пользователь! Введите число от 1 до 7, чтобы узнать сколько дней осталось до выходного: ");
+                int day;
+                if (!int.TryParse(Console.ReadLine(), out day) || day < 1 || day > 7)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Вы ввели неправильное число.");
+                    continue;
+                }
+                if (day == 7)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Поздравляем! Это воскресение и у вас выходной!");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Это {daysOfWeek[day - 1]}. Осталось {7-day} дней до выходного");
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Хотите продолжить (да/нет)? ");
+                if (Console.ReadLine() != "да")
+                {
+                    break;
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("До свидания!");
+            Console.ReadLine()
+        }
+    }
+}
